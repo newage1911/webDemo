d2185bb68c222515ed0a6c4f0029f90eb9cbebc8
newage1911 <58944650+newage1911@users.noreply.github.com>
2021-10-17 21:36:55 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ศูนย์พัฒนาและประเมินคุณภาพการให้บริการอาชีวเวชกรรมและเวชกรรมสิ่งแวดล้อม</title>
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Oswald'>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+    crossorigin="anonymous"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Sarabun&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="myQuestion.css">
+  <link rel="icon" type="image/png" href="image/public.png">
+  <style>
+    body {
+      margin: 0;
+      font-family: 'Sarabun', sans-serif;
+    }
+
+    .navbar-custom1 {
+      background-color: #215D34;
+    }
+
+    .htop {
+      position: fixed;
+      width: 100%;
+    }
+
+    /* Style the top navigation bar */
+    .topnav {
+      overflow: hidden;
+      background-color: #333;
+    }
+
+    /* Style the topnav links */
+    .topnav a {
+      float: left;
+      display: block;
+      color: #f2f2f2;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+    }
+
+    /* Change color on hover */
+    .topnav a:hover {
+      background-color: #ddd;
+      color: black;
+    }
+
+    ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      overflow: hidden;
+      border: 1px solid #e7e7e7;
+      background-color: #f3f3f3;
+    }
+
+    li {
+      float: left;
+    }
+
+    li a {
+      display: block;
+      color: #666;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+    }
+
+    li a:hover:not(.active) {
+      background-color: #ddd;
+      color: #2f844a;
+    }
+
+    li a.active {
+      color: white;
+      background-color: #04AA6D;
+    }
+
+    .footer {
+      left: 0;
+      bottom: 0;
+      width: 100%;
+      background-color: #215D34;
+      color: white;
+      text-align: center;
+    }
+
+    .center {
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+    }
+
+  </style>
+</head>
+
+<body>
+  <header class="fixed-top">
+    <div class="container-fixed-top">
+      <nav class="navbar navbar-expand-lg navbar-light navbar-custom1">
+        <div class="container" style="padding-top: 10px;">
+          <div class="row">
+            <div class="col-1">
+              <a class="navbar-brand" href="index.php">
+                <img src="image/public.png" alt="" width="100" height="auto" class="d-inline-block align-top">
+              </a>
+            </div>
+            <div class="col-11" style="padding-top: 35px;">
+              <b
+                style="color: rgb(255, 254, 254); font-size: 28px ; padding-left: 80px; padding-right: 10px;">ศูนย์พัฒนาและประเมินคุณภาพการให้บริการอาชีวเวชกรรมและเวชกรรมสิ่งแวดล้อม</b>
+              </a>
+            </div>
+          </div>
+        </div>
+    </div>
+    </nav>
+    <!--Menu zone-->
+    <ul>
+      <li><a class="active" href="#">หน้าหลัก</a></li>
+      <li><a href="Agency1.php">คณะกรรมการ</a></li>
+      <li><a href="Law.php">กฎหมาย</a></li>
+      <li><a href="Question1.php">คำถามที่พบบ่อย</a></li>
+    </ul>
+    </div>
+  </header>
+
+  <div class="container" style="margin-top: 230px; padding-left: 200px; padding-right: 220px;">
+    <p style=" font-size: 18px; background-color: rgba(135, 206, 250, 0.421); text-align: center;">การขอขึ้นทะเบียน
+      การขึ้นทะเบียน อายุและการต่ออายุการขึ้นทะเบียน การออกใบแทนการขึ้นทะเบียน การเพิกถอนทะเบียน
+      และการประเมินคุณภาพการให้บริการที่ได้ขึ้นทะเบียน</p>
+  </div>
+  <div class="container" style="margin-top: 50px; padding: 2px 20px;">
+    <div class="row" style="margin-top: 20px;margin-bottom: 30px;">
+      <div class="col-2"></div>
+      <div class="col-3">
+        <a href="Feature1.html"><img class="center" id="" src="image/check.png" alt="4"
+            style=" border-radius: 1.5%;"></a>
+        <p style="text-align: center; margin-top: 20px;">ตรวจสอบประเภทการยื่น</p>
+      </div>
+      <div class="col-2">
+        <a href="Flow.html"><img class="center" id="" src="image/steps.png" alt="4"
+            style=" border-radius: 1.5%;"></a>
+        <p style="text-align: center; margin-top: 20px;">ขั้นตอนการพิจารณา</p>
+      </div>
+      <div class="col-3">
+        <a href="Form1.html"><img class="center" id="" src="image/web.png" alt="4"
+            style=" border-radius: 1.5%;"></a>
+        <p style="text-align: center; margin-top: 20px;">แบบฟอร์ม</p>
+      </div>
+      <div class="col-2"></div>
+    </div>
+    <div class="row" style="margin-top: 50px; margin-bottom: 100px;">
+      <div class="col-2"></div>
+      <div class="col-3">
+        <a href="Document.html"><img class="center" id="" src="image/folder.png" alt="4"
+            style=" border-radius: 1.5%;"></a>
+        <p style="text-align: center; margin-top: 20px;">ส่งเอกสาร</p>
+      </div>
+      <div class="col-2">
+        <a href="https://docs.google.com/spreadsheets/d/154g3LIcYi7woX24XuVW54tpG3NHnsQUCgOGsr0w4NE4/edit#gid=0"><img class="center" id="" src="image/agency.png" alt="4"
+            style=" border-radius: 1.5%;"></a>
+        <p style="text-align: center; margin-top: 20px;">หน่วยบริการที่ขึ้นทะเบียน</p>
+      </div>
+      <div class="col-3">
+        <a href="https://docs.google.com/spreadsheets/u/2/d/1KtBBGh8eK8hVYpc6Ila98XFTGoZphsFBdbbHkbRt9g0/edit?usp=drive_web&ouid=116448946940334163496"><img class="center" id="" src="image/status.png" alt="4"
+            style=" border-radius: 1.5%;"></a>
+        <p style="text-align: center; margin-top: 20px;">ตรวจสอบสถานะ</p>
+      </div>
+      <div class="col-2"></div>
+    </div>
+  </div>
+
+  <div class="container" style="margin-top: 100x; padding-left: 500px; padding-right: 520px;">
+    <p style=" font-size: 16px; background-color: rgba(135, 206, 250, 0.421); text-align: center;">ข่าวประชาสัมพันธ์</p>
+  </div>
+
+  <!--Footer-->
+  <div class="footer">
+    <div class="container">
+      <div class="row row-30">
+        <div class="col-md-4 col-xl-5">
+          <br>
+          <div class="pr-xl-4">
+            <a class="brand" href="index.php">
+              <img class="brand-logo-light" src="image/public.png" alt="" width="80" height="auto">
+            </a>
+            <dd style="font-size: 14px;">ศูนย์พัฒนาและประเมินคุณภาพการให้บริการอาชีวเวชกรรมและเวชกรรมสิ่งแวดล้อม</dd>
+            <!-- Rights-->
+            <p class="rights" style="font-size: 12px;"><span>© </span><span class="copyright-year">2020</span><span>
+              </span><span>Company</span><span> - </span><span>All Rights Reserved.</span></p>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <br>
+          <br>
+          <b style="font-size: 14px;">Contacts</b>
+          <dl class="contact-list">
+            <dt style="font-size: 14px;">Address</dt>
+            <dd style="font-size: 13px;">อาคารเวชศาสตร์อาชีพและสิ่งแวดล้อม ชั้น 2</dd>
+            <dd style="font-size: 13px;">98/2 หมู่ 8 ตำบล สำโรงกลาง อำเภอ พระประแดง จังหวัด สมุทรปราการ 10130</dd>
+          </dl>
+        </div>
+        <div class="col-md-4 col-xl-3">
+          <br>
+          <br>
+          <dl class="contact-list" style="font-size: 14px;">
+            <dt style="font-size: 13px;">Email</dt>
+            <dd style="font-size: 13px;">
+              <p>oshspk@gmail.com</p>
+            </dd>
+          </dl>
+          <dl class="contact-list">
+            <dt style="font-size: 14px;">Phones</dt>
+            <dd style="font-size: 13px;">02 394 0166 , 02 394 7936 ต่อ 203</dd>
+            </dd>
+          </dl>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+</body>
+
+</html>
